9cfeb4be85304d0ebfb53a2453c0d17dcff33a7d
Shajadul <shajadulrahe@gmail.com>
2022-12-11 07:44:57 +0600

    Feat: Add form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import SimpleInput from "./components/SimpleInput";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <SimpleInput />
+    </div>
+  );
 }
 
 export default App;
