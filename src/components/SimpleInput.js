91bb67c46bf16b9636211e167044eba6812f4392
Shajadul <shajadulrahe@gmail.com>
2022-12-11 08:32:12 +0600

    Feat: Add validation to form

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,10 +1,37 @@
+import React, { useState, useRef } from "react";
 const SimpleInput = (props) => {
+  const gettingInput = useRef();
+  const [enteredInput, setInput] = useState("");
+  const [isValid, setIsValid] = useState(true);
+  const inputHandle = (event) => {
+    setInput(event.target.value);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (enteredInput.trim() === "") {
+      setIsValid(false);
+      return;
+    }
+    setIsValid(true);
+    console.log(enteredInput);
+    const gotInput = gettingInput.current.value;
+    console.log(gotInput);
+    setInput("");
+  };
+  const formClass = isValid ? "form-control" : "form-control invalid";
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={submitHandler}>
+      <div className={formClass}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={gettingInput}
+          onChange={inputHandle}
+          type="text"
+          id="name"
+          value={enteredInput}
+        />
       </div>
+      {!isValid && <p className="error-text">The input is invalid!</p>}
       <div className="form-actions">
         <button>Submit</button>
       </div>
